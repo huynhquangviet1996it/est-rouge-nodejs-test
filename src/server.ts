161422be8ef64536b5a6c1620e0af851021a289e
huynhquangviet1996it <huynhquangviet1996it@gmail.com>
2020-11-11 00:17:52 +0700

    feat: add environment.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from './app';
+import env from './environment';
 
-const PORT = 3000;
+const PORT = env.getPort();
 
 app.listen(PORT, () => {
   console.log('Express server listening on port ' + PORT);
